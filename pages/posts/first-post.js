cd41981b6ff5acb8d47456dae76115061b40204d
Tio Saputra <tiosaputra@Tios-MacBook-Pro.local>
2020-05-18 04:27:41 +0700

    staticly render page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+export default function FristPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Posts</h1>
+
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
+}
